bf77b0ae0fbaec57410c08db5f0f3d5f34b51a2b
Robertus Spimbley <robertstamper39@gmail.com>
2019-03-24 11:58:51 +0000

    fixed it.. fb seemed to change the html lauyout a bit. this is completely hardcoded to the specific html structure. i should probably attempt to make it a bit smarter at some point

--- content_script.js
+++ content_script.js
@@ -3,8 +3,8 @@ var config = { attributes: true, childList: true, characterData: true, subtree:t
 
 function sort_marketboxes(a,b)
 {
-    aText = a.childNodes[0].childNodes[1].childNodes[1].innerText.split('·')[1].trim();
-    bText = b.childNodes[0].childNodes[1].childNodes[1].innerText.split('·')[1].trim();
+    aText = a.childNodes[0].childNodes[0].childNodes[1].childNodes[1].innerText.split('·')[1].trim();
+    bText = b.childNodes[0].childNodes[0].childNodes[1].childNodes[1].innerText.split('·')[1].trim();
     if(convertTimeToNumber(aText) < convertTimeToNumber(bText))
         return -1;
     else    
@@ -44,15 +44,19 @@ var observer = new MutationObserver(function(mutations){
         {
             currentNumBoxes = marketboxes.length;   
         }
+        else
+        {
+            return;
+        }
         if(currentNumBoxes > 0)
         {
-            var grps = [[marketboxes[0]]];
+            var grps = [[marketboxes[0].parentElement]];
             
             for(let i=1;i<marketboxes.length;i++)
             {
-                if(marketboxes[i].parentElement == grps[grps.length-1][0].parentElement)
+                if(marketboxes[i].parentElement.parentElement == grps[grps.length-1][0].parentElement)
                 {   
-                    grps[grps.length-1].push(marketboxes[i])
+                    grps[grps.length-1].push(marketboxes[i].parentElement)
                 }
                 else
                 {
@@ -64,7 +68,8 @@ var observer = new MutationObserver(function(mutations){
 
             grps.forEach((g)=>{
                 let parentElement = g[0].parentNode;
-                $(g).sort(sort_marketboxes).prependTo(parentElement);    
+                if(g.length > 1)
+                    $(g).sort(sort_marketboxes).prependTo(parentElement);    
             });
 
             observer.observe(document.body, config);                    
